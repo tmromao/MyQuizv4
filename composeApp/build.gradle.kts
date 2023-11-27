919ed3c361fa9149133e4b9f530b932f78b098d8
tromao <tiagomromao@gmail.com>
2023-11-27 22:29:25 +0000

    Adding sqldelight library

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -7,6 +7,7 @@ plugins {
     alias(libs.plugins.jetbrainsCompose)
     alias(libs.plugins.kotlinxSerialization)
     alias(libs.plugins.mokoResources)
+    alias(libs.plugins.sqldelight)
 }
 
 kotlin {
