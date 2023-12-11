e7589522c95e8426c7540f380b4b84f691177b76
tromao <tiagomromao@gmail.com>
2023-12-11 22:15:18 +0000

    Upgrade voyager lib and refactor screen model

--- composeApp/src/commonMain/kotlin/com/jetbrains/kmpapp/screens/components/QuestionScreenModel.kt
+++ composeApp/src/commonMain/kotlin/com/jetbrains/kmpapp/screens/components/QuestionScreenModel.kt
@@ -1,7 +1,6 @@
 package com.jetbrains.kmpapp.screens.components
 
 import cafe.adriel.voyager.core.model.ScreenModel
-import cafe.adriel.voyager.core.model.coroutineScope
 import cafe.adriel.voyager.core.model.screenModelScope
 import com.jetbrains.kmpapp.data.CacheDataImp
 import com.jetbrains.kmpapp.data.QuestionListEvent
@@ -15,33 +14,24 @@ class QuestionScreenModel(
     private val questionDataSource: CacheDataImp,
 ) : ScreenModel {
 
+
     private val _state = MutableStateFlow(QuestionListState(
         questions = questions
     ))
     val uiState = _state.asStateFlow()
-
-   /* init {
+    init {
         screenModelScope.launch {
 
-            //TODO: this is not working
-            uiState.value.copy(
-                questions = questionDataSource.getQuestions().first()
-            )
-            *//*state = combine(
-                _state,
-                questionDataSource.getQuestions(),
-            ) { state, questions ->
-                state.copy(
-                    questions = questions
+            questionDataSource.insertQuestion(
+                QuestionObject(
+                    id = 1,
+                    question = "Question 1",
+                    answer = "Answer 1"
                 )
-            }.stateIn(
-                scope = screenModelScope,
-                started = SharingStarted.WhileSubscribed(5000L),
-                initialValue = QuestionListState()
-            ) as MutableStateFlow<QuestionListState>*//*
+            )
         }
 
-    }*/
+    }
 
     //fun getObject() : Flow<QuestionObject?> = _state.asStateFlow()
     fun getObject(): QuestionObject = QuestionObject(
@@ -49,6 +39,15 @@ class QuestionScreenModel(
         question = "Question 1",
         answer = "Answer 1"
     )
+
+    fun getQuestions() {
+        screenModelScope.launch {
+            _state.value = _state.value.copy(
+                questions = questionDataSource
+                    .getQuestions().first()
+            )
+        }
+    }
 
     fun onEvent(event: QuestionListEvent) {
 
